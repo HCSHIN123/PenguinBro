6ff9f5a2ed6f536ce386ae03ddeda31326916bb2
Nubox <sjhbelieve@gmail.com>
2024-06-28 10:08:30 +0900

    Clear Main hub

--- Assets/Scripts/CannonScripts/BulletScripts/Bullet_DistributedAttack.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class Bullet_DistributedAttack : Bullet
-{
-    protected Transform[] bulletTransforms;
-    protected bool isSplit = false;
-}
